d50afdac4be263f003e251b34794413d26b83a21
Marko Atanasievski <marko.atanasievski@cartesi.io>
2022-03-11 14:54:27 +0100

    Move rollup http client to separate crate

--- linux/rollup/http/echo-dapp/build.sh
+++ linux/rollup/http/echo-dapp/build.sh
@@ -2,12 +2,11 @@
 
 TOOLCHAIN_IMAGE="${TOOLCHAIN_IMAGE:-cartesi/toolchain}"
 TOOLCHAIN_TAG="${TOOLCHAIN_TAG:-0.8.0}"
-CONTAINER_BASE="${CONTAINER_BASE:-/opt/cartesi}"
+CONTAINER_BASE="${CONTAINER_BASE:-/opt/cartesi/tools}"
 
-
-docker run --rm -e USER=`id -u -n` -e GROUP=`id -g -n` -e UID=`id -u` -e GID=`id -g` -v `pwd`:$CONTAINER_BASE \
+docker run --rm -e USER=`id -u -n` -e GROUP=`id -g -n` -e UID=`id -u` -e GID=`id -g` -v `pwd`/../:$CONTAINER_BASE \
 --env CC=riscv64-cartesi-linux-gnu-gcc --env CXX=riscv64-cartesi-linux-gnu-g++ --env CFLAGS="-march=rv64ima -mabi=lp64" \
--w $CONTAINER_BASE $TOOLCHAIN_IMAGE:$TOOLCHAIN_TAG \
+-w $CONTAINER_BASE/echo-dapp $TOOLCHAIN_IMAGE:$TOOLCHAIN_TAG \
 cargo build -Z build-std=std,core,alloc,panic_abort,proc_macro --target riscv64ima-cartesi-linux-gnu.json --release
 
 
