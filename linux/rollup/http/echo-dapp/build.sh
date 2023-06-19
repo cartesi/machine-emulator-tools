3490a8f71e11429d22e4e09bac5a34afaf97030e
Victor Fusco <1221933+vfusco@users.noreply.github.com>
2023-06-19 11:40:27 -0300

    feat!: Update toolchain to 0.15.0

--- linux/rollup/http/echo-dapp/build.sh
+++ linux/rollup/http/echo-dapp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOOLCHAIN_IMAGE="${TOOLCHAIN_IMAGE:-cartesi/toolchain}"
-TOOLCHAIN_TAG="${TOOLCHAIN_TAG:-0.14.0}"
+TOOLCHAIN_TAG="${TOOLCHAIN_TAG:-0.15.0}"
 RISCV_ARCH="${RISCV_ARCH:-rv64gc}"
 RISCV_ABI="${RISCV_ABI:-lp64d}"
 CONTAINER_BASE="${CONTAINER_BASE:-/opt/cartesi/tools}"
@@ -9,4 +9,4 @@ CONTAINER_BASE="${CONTAINER_BASE:-/opt/cartesi/tools}"
 docker run --rm -e USER=`id -u -n` -e GROUP=`id -g -n` -e UID=`id -u` -e GID=`id -g` -v `pwd`/../:$CONTAINER_BASE \
 --env CC=riscv64-cartesi-linux-gnu-gcc --env CXX=riscv64-cartesi-linux-gnu-g++ --env CFLAGS="-march=$RISCV_ARCH -mabi=$RISCV_ABI" \
 -w $CONTAINER_BASE/echo-dapp $TOOLCHAIN_IMAGE:$TOOLCHAIN_TAG \
-cargo build -Z build-std=std,core,alloc,panic_abort,proc_macro --target rv64gc-cartesi-linux-gnu.json --release
+cargo build --target riscv64gc-unknown-linux-gnu --release
