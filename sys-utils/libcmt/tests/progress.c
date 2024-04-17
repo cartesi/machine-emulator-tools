bf8edec561cf9b7860253f4fd2bf4e4c6ad86d22
Marcelo Politzer <251334+mpolitzer@users.noreply.github.com>
2024-04-17 18:40:45 -0300

    feat: add progress test

---
+++ sys-utils/libcmt/tests/progress.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "rollup.h"
+
+int main(void) {
+    cmt_rollup_t rollup;
+
+    assert(cmt_rollup_init(&rollup) == 0);
+    assert(cmt_rollup_progress(&rollup, 0) == 0);
+    assert(cmt_rollup_progress(&rollup, 10) == 0);
+    assert(cmt_rollup_progress(&rollup, 100) == 0);
+    assert(cmt_rollup_progress(&rollup, 1000) == 0);
+    assert(cmt_rollup_progress(&rollup, UINT32_MAX) == 0);
+    cmt_rollup_fini(&rollup);
+
+    printf("test_rollup_progress passed!\n");
+    return 0;
+}
